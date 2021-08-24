3428ba0f328c6a672a155d88aad2c20bad37cd10
Akshay-rb <akshayrbr2@gmail.com>
2021-08-24 12:23:16 +0530

    init: created groot app

---
+++ app.js
@@ -0,0 +1,29 @@
+const btnTranslater = document.querySelector("#btn-translate")
+const txtInput = document.querySelector('#txt-input')
+const outputDiv = document.querySelector('#output')
+// var url = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.jsons"
+// var url = "https://api.funtranslations.com/translate/minion.json"
+let url = 'https://api.funtranslations.com/translate/groot.json'
+
+// url construction
+function getURL(text){
+    return url + "?text=" +text
+}
+
+//error handling function
+function errorHandler(error){
+    console.log('an error occured', error)
+    alert('somethingwrong')
+}
+
+// click handling function
+function clickEventHandler() {
+    fetch(getURL(txtInput.value))
+    .then(response => response.json())
+    // .then(json => console.log(json.contents.translated))
+    .then(json => outputDiv.innerText = json.contents.translated)
+    .catch(errorHandler)
+}
+
+//calling the clickhandler on click
+btnTranslater.addEventListener('click', clickEventHandler)
